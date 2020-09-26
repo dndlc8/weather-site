1e46bb69cbe8ca6c16e936ec4403134c4e4a25b1
Dominique De La Cruz <dndlc8@gmail.com>
2020-09-26 15:37:17 -0700

    website creation

---
+++ weather_api/src/index.js
@@ -0,0 +1,145 @@
+//import "./styles.css";
+
+//Date and Time Header
+let now = new Date();
+let hours = now.getHours();
+let minutes = ("0" + now.getMinutes()).slice(-2);
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+let day = days[now.getDay()];
+let date = now.getDate();
+let year = now.getFullYear();
+
+let h2 = document.querySelector("h2");
+h2.innerHTML =
+  `${day} ${month}/${date}/${year}` + "<br>" + `${hours}:${minutes}`;
+
+//Los Angeles Temps
+
+let lat = 34.052;
+let lon = -118.244;
+let units = "imperial";
+let apiKey = "1b9a19801a7a96280358cc2498e9820b";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(losAngTemp);
+
+function losAngTemp(response) {
+  let temp = Math.round(response.data.main.temp);
+  let currentTemp = document.querySelector(".temp");
+  currentTemp.innerHTML = temp;
+
+  function faren(event) {
+    event.preventDefault(event);
+    let currentTemp = document.querySelector(".temp");
+    currentTemp.innerHTML = temp;
+  }
+  let farenheit = document.querySelector(".farenheit");
+  farenheit.addEventListener("click", faren);
+
+  function cels(event) {
+    event.preventDefault();
+    let currentTemp = document.querySelector(".temp");
+    celsTemp = Math.round(((temp - 32) * 5) / 9);
+    currentTemp.innerHTML = celsTemp;
+  }
+  let celsius = document.querySelector(".celsius");
+  celsius.addEventListener("click", cels);
+}
+
+//Current Temps
+
+function searchNow(event) {
+  event.preventDefault();
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `Currently, Where You Are`;
+
+  function showPosition(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let units = "imperial";
+    let apiKey = "1b9a19801a7a96280358cc2498e9820b";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(showTemp);
+
+    function showTemp(response) {
+      let temp = Math.round(response.data.main.temp);
+      let currentTemp = document.querySelector(".temp");
+      currentTemp.innerHTML = temp;
+
+      function faren(event) {
+        event.preventDefault(event);
+        let currentTemp = document.querySelector(".temp");
+        currentTemp.innerHTML = temp;
+      }
+      let farenheit = document.querySelector(".farenheit");
+      farenheit.addEventListener("click", faren);
+
+      function cels(event) {
+        event.preventDefault();
+        let currentTemp = document.querySelector(".temp");
+        let celsTemp = Math.round(((temp - 32) * 5) / 9);
+        currentTemp.innerHTML = celsTemp;
+      }
+      let celsius = document.querySelector(".celsius");
+      celsius.addEventListener("click", cels);
+    }
+  }
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let currentButton = document.querySelector(".btn-info");
+currentButton.addEventListener("click", searchNow);
+
+//Search Temps
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchInput.value}`;
+
+  let city = document.querySelector("#search-text-input");
+  let units = "imperial";
+  let apiKey = "1b9a19801a7a96280358cc2498e9820b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=${units}`;
+
+  function searchTemp(response) {
+    let temp = Math.round(response.data.main.temp);
+    let currentTemp = document.querySelector(".temp");
+    currentTemp.innerHTML = temp;
+
+    function faren(event) {
+      event.preventDefault(event);
+      let currentTemp = document.querySelector(".temp");
+      currentTemp.innerHTML = temp;
+    }
+    let farenheit = document.querySelector(".farenheit");
+    farenheit.addEventListener("click", faren);
+
+    function cels(event) {
+      event.preventDefault();
+      let currentTemp = document.querySelector(".temp");
+      let celsTemp = Math.round(((temp - 32) * 5) / 9);
+      currentTemp.innerHTML = celsTemp;
+    }
+    let celsius = document.querySelector(".celsius");
+    celsius.addEventListener("click", cels);
+  }
+  axios.get(apiUrl).then(searchTemp);
+}
+let searchButton = document.querySelector(".btn-primary");
+searchButton.addEventListener("click", searchCity);
